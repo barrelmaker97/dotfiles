ad090a6d7c6d07e568b43020fbdc774869b19fc6
Nolan Cooper <nolancooper97@gmail.com>
2016-11-29 15:01:47 -0600

    Integrate battery into status bar

--- bin/battery.sh
+++ bin/battery.sh
@@ -1,1 +1,2 @@
-/mnt/c/Windows/System32/cmd.exe /cWMIC Path Win32_Battery Get EstimatedChargeRemaining | sed -n '2 p' | cut -c1-2
+#!/bin/bash
+/mnt/c/Windows/System32/cmd.exe /cWMIC Path Win32_Battery Get EstimatedChargeRemaining | sed -n '2 p' | cut -c1-3 | awk '{$1=$1};1'
