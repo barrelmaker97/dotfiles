e02a796e7a63916d7a0f690dabf2a9b16d962afa
Nolan Cooper <nolancooper97@gmail.com>
2016-12-10 23:09:44 -0600

    Shorten command path

--- bin/battery.sh
+++ bin/battery.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/mnt/c/Windows/System32/cmd.exe /cWMIC Path Win32_Battery Get EstimatedChargeRemaining | sed -n '2 p' | cut -c1-3 | awk '{$1=$1};1'
+cmd.exe /cWMIC Path Win32_Battery Get EstimatedChargeRemaining | sed -n '2 p' | cut -c1-3 | awk '{$1=$1};1'
