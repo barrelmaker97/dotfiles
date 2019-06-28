33fc9859fc1bfb86ce45d50c77d8566caccd03b5
Nolan Cooper <nolancooper97@gmail.com>
2019-06-27 23:39:18 -0500

    Improved sed portion of git log command

--- bin/log.sh
+++ bin/log.sh
@@ -1,4 +1,4 @@
 git log --all --graph --color=always \
-	--pretty=format:"GPGSIG%G?%C(red)%h%C(green)%h%C(yellow)%h%C(reset) -%C(auto)%d%C(reset) %s %C(green)(%cr) %C(bold blue)<%an>%C(reset)" \
-	| sed -e 's/GPGSIGN\(.\{12\}\)\(.\{12\}\)\(.\{12\}\)/\1/; s/GPGSIGG\(.\{12\}\)\(.\{12\}\)\(.\{12\}\)/\2/; s/GPGSIG.\(.\{12\}\)\(.\{12\}\)\(.\{12\}\)/\3/;' \
+	--pretty=format:"GPGSIG%G?%C(red)%h%C(reset) -%C(auto)%d%C(reset) %s %C(green)(%cr) %C(bold blue)<%an>%C(reset)" \
+	| sed -e 's/GPGSIGN\(...\)./\11/; s/GPGSIGG\(...\)./\12/; s/GPGSIG.\(...\)./\13/;' \
 	| less -RX
