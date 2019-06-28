5ffae1d58e009cf9513afdf8a3310db458f5007c
Nolan Cooper <nolancooper97@gmail.com>
2019-06-28 00:07:13 -0500

    Moved improved git log command to an alias

--- bin/log.sh
+++
@@ -1,4 +0,0 @@
-git log --all --graph --color=always \
-	--pretty=format:"GPGSIG%G?%C(red)%h%C(reset) -%C(auto)%d%C(reset) %s %C(green)(%cr) %C(bold blue)<%an>%C(reset)" \
-	| sed -e 's/GPGSIGN\(...\)./\11/; s/GPGSIGG\(...\)./\12/; s/GPGSIG.\(...\)./\13/;' \
-	| less -RX
