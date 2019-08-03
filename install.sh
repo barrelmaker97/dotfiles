a0b9284f05b02c558fc1c027045d999df4fd818a
Nolan Cooper <nolancooper97@gmail.com>
2019-08-03 14:53:01 -0500

    Check for git and existing repo

--- install.sh
+++ install.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+which git
+if [ $? -eq 1 ]; then
+	echo -e "Git is not installed or cannot be found on this system"
+	exit 1
+fi
+
 cd ~
-git clone https://github.com/barrelmaker97/dotfiles
-cd dotfiles
+if [ -d dotfiles ]; then
+	cd dotfiles && git checkout master && git pull
+else
+	git clone https://github.com/barrelmaker97/dotfiles
+	cd dotfiles
+fi
 ./setup.sh all
