63638a5dc1ceee9a2115903f2a9ce467b03bb53b
Nolan Cooper <nolancooper97@gmail.com>
2019-08-03 14:01:44 -0500

    Improve tmux scripts

--- bin/battery.sh
+++ bin/battery.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-cat /sys/class/power_supply/BAT0/capacity
+linux=/sys/class/power_supply/BAT0/capacity
+wsl=/sys/class/power_supply/battery/capacity
+echo -e "$(cat $linux $wsl 2> /dev/null)% "
