520f0c3d49ce36e8bcc253adb879479492b827a4
Nolan Cooper <nolancooper97@gmail.com>
2019-10-18 16:48:46 -0500

    Fix linter error

--- bin/max.sh
+++ bin/max.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Max brightness when run without arguments
 if [ "$#" -eq 0 ]; then
-	sudo tee /sys/class/backlight/intel_backlight/brightness < /sys/class/backlight/intel_backlight/max_brightness
+	sudo cat /sys/class/backlight/intel_backlight/max_brightness | sudo tee /sys/class/backlight/intel_backlight/brightness
 	exit 0
 fi
