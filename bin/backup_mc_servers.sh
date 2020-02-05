4b2ba3cd40f9efd4f5b52da1fd0ba7a992593737
Nolan Cooper <nolancooper97@gmail.com>
2020-02-04 21:09:52 -0600

    Don't backup if one has already been done today

--- bin/backup_mc_servers.sh
+++ bin/backup_mc_servers.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+BACKUP_HOST="pi@pollux"
 FILENAME="mc-servers-$(date +%m-%d-%Y).zip"
-zip -r /tmp/"${FILENAME}" /home/barrelmaker/*craft
-rsync -a /tmp/"${FILENAME}" pi@pollux:/mnt/Backup/
-rm -rf /tmp/"${FILENAME}"
+BACKUP_LOCATION="/mnt/Backup"
+
+backup ()
+{
+	zip -r /tmp/"${FILENAME}" /home/barrelmaker/*craft
+	rsync -a /tmp/"${FILENAME}" "${BACKUP_HOST}":/mnt/Backup/
+	rm -rf /tmp/"${FILENAME}"
+}
+
+ssh -q ${BACKUP_HOST} [[ -f "${BACKUP_LOCATION}/${FILENAME}" ]] && echo "Backup for today already complete" || backup
