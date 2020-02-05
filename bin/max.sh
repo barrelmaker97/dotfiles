3e684263dd1d447b7ce5756914f84bb1c1abca3b
Nolan Cooper <nolancooper97@gmail.com>
2020-02-05 11:24:57 -0600

    Don't need brightness script anymore

--- bin/max.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Max brightness when run without arguments
-if [ "$#" -eq 0 ]; then
-	sudo cat /sys/class/backlight/intel_backlight/max_brightness | sudo tee /sys/class/backlight/intel_backlight/brightness
-	exit 0
-fi
