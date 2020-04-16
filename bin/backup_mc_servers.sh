d7571a8241a3a59d1edb31ef98e04e51192c41b7
Nolan Cooper <nolancooper97@gmail.com>
2020-04-15 20:11:08 -0500

    Update mc-server backup script

--- bin/backup_mc_servers.sh
+++ bin/backup_mc_servers.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 BACKUP_HOST="pi@pollux"
 FILENAME="mc-servers-$(date +%m-%d-%Y).zip"
-BACKUP_LOCATION="/mnt/Backup/mc-servers"
+BACKUP_DESTINATION="/mnt/Backup/mc-servers"
+BACKUP_SOURCE="/home/barrelmaker/mc-servers"
 
 backup ()
 {
-	zip -r /tmp/"${FILENAME}" /home/barrelmaker/*craft
-	rsync -a /tmp/"${FILENAME}" "${BACKUP_HOST}":"${BACKUP_LOCATION}"
+	cd "${BACKUP_SOURCE}" || exit
+	zip -r /tmp/"${FILENAME}" ./*
+	rsync -a /tmp/"${FILENAME}" "${BACKUP_HOST}":"${BACKUP_DESTINATION}"
 	rm -rf /tmp/"${FILENAME}"
 }
 
-if ssh -q ${BACKUP_HOST} [[ -f "${BACKUP_LOCATION}/${FILENAME}" ]]
+if ssh -q ${BACKUP_HOST} [[ -f "${BACKUP_DESTINATION}/${FILENAME}" ]]
 then
 	echo "Backup for today already complete"
 else
