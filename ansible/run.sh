a4aa9921699b962ae4bfbe483df2f7c83b11317d
Nolan Cooper <nolancooper97@gmail.com>
2021-03-18 17:48:52 -0500

    Add ansible stuff here for now

---
+++ ansible/run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+RED='\e[1;31m'
+GREEN='\e[1;32m'
+RESET='\e[0m'
+
+TICK="\\r[${GREEN}✓${RESET}]"
+CROSS="\\r[${RED}✗${RESET}]"
+INFO="\\r[i]"
+DEPENDENCIES=(ansible-playbook)
+
+check_dependencies ()
+{
+	echo -ne "${INFO} Checking for dependencies..."
+	for DEPENDENCY in "${DEPENDENCIES[@]}"
+	do
+		command -v "${DEPENDENCY}" > /dev/null
+		EXIT_CODE=$?
+		if [ $EXIT_CODE -ne 0 ]; then
+			echo -e "${CROSS} ${DEPENDENCY} is not installed or cannot be found on this system"
+			exit $EXIT_CODE
+		fi
+	done
+	echo -e "${TICK}"
+}
+
+check_dependencies
+read -rp "IP: " IP
+read -rp "Username (Default barrelmaker): " USERNAME
+USERNAME=${USERNAME:-barrelmaker}
+echo -e "Starting Deployment Playbook..."
+echo server ansible_host="${IP}" ansible_user="${USERNAME}" > inventory_hostname
+ansible-playbook test.yaml -i inventory_hostname -v
+rm inventory_hostname
