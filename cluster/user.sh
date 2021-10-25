4377a1459473886ac1da67f50c0536c25e72a064
Nolan Cooper <nolancooper97@gmail.com>
2021-10-24 23:20:46 -0500

    More example commands

--- cluster/user.sh
+++ cluster/user.sh
@@ -1,1 +1,14 @@
+# Create private key and CSR
 openssl req -new -newkey rsa:2048 -nodes -keyout barrelmaker.key -out barrelmaker.csr -subj "/CN=barrelmaker"
+
+# Convert CSR to base64 string
+cat barrelmaker.csr | base64 | tr -d "\n"
+
+# Get cert from CSR resource
+kubectl get csr barrelmaker -o jsonpath='{.status.certificate}'| base64 -d > barrelmaker.crt
+
+# Create user in kubectl
+kubectl config set-credentials barrelmaker --client-key=barrelmaker.key --client-certificate=barrelmaker.crt --embed-certs=true
+
+# Create context in kubectl
+kubectl config set-context barrelmaker --cluster=kubernetes --user=barrelmaker --namespace=barrelmaker
